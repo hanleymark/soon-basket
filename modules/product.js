37ebfb56eb68da8d5699704111f1dfa946310146
Mark hanley <home@markhanley.co.uk>
2023-03-14 14:48:55 +0000

    Add site heading and basket icon
    Relates #3

--- modules/product.js
+++ modules/product.js
@@ -1,12 +1,12 @@
 export class Product {
-    constructor(id, price, title, description) {
+    constructor(id, price, title, description, units) {
         this.id = id;
         this.price = price;
         this.title = title;
         this.description = description;
     }
 
-    static baseUrl = './images/products/';
+    static baseUrl = './resources/product-images/';
 
     get imageUrl() {
         return `${this.baseUrl}${id}.png`;
