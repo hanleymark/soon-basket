c5d5a6a018c1bc65f03a4b7f1cd942029fe6a628
Mark hanley <home@markhanley.co.uk>
2023-03-16 10:13:39 +0000

    Add and display sample product inventory
    Relates #3

--- app.js
+++ app.js
@@ -1,3 +1,53 @@
 import { Product } from "./modules/product.js";
 import { Basket } from "./modules/basket.js";
 
+// Get page element references
+const productDetailsPlaceholder = document.querySelector(
+  "#product-details-placeholder"
+);
+
+// Sample product data to populate product inventory
+const sampleProductData = [
+  {
+    id: "0001",
+    price: 19.95,
+    title: "'Take the Trails' Cotton T Shirt",
+    description:
+      "Introducing our stylish and comfortable cotton t-shirt with a bold and adventurous message - 'Take the Trails'. Made from premium quality cotton, this t-shirt features a classic fit and is perfect for casual outings, hikes, or any outdoor adventure. The printed design adds a touch of wanderlust to your wardrobe and inspires you to explore the great outdoors. So, whether you're hitting the trails or simply running errands, this t-shirt is the perfect addition to your collection.",
+    },
+  {
+    id: "0002",
+    price: 229.5,
+    title: "Goose Down Red Puffer Jacket",
+    description:
+      "Introducing our stunning Goose Down Red Puffer Jacket, a must-have addition to your winter wardrobe. This jacket is the perfect combination of style, comfort, and functionality, making it an ideal choice for anyone who wants to stay warm and fashionable during the colder months. Made from high-quality materials and filled with premium goose down feathers, this jacket features a durable and water-resistant outer layer that will protect you from the elements, while the soft and cozy inner lining will keep you warm and comfortable.",
+    },
+  {
+    id: "0003",
+    price: 44.25,
+    title: "Turquoise Snoopy Print Sweatshirt",
+    description:
+      "Introducing our vibrant and cozy turquoise sweatshirt, featuring a stylish black Snoopy printed design that is sure to add a touch of fun and whimsy to your wardrobe. Made from high-quality and soft materials, this sweatshirt is perfect for both casual and comfortable wear, whether you're lounging at home or running errands around town. The classic crew neck and ribbed cuffs and hem ensure a comfortable and snug fit, while the bright turquoise adds a pop of color to any outfit.",
+    },
+  {
+    id: "0004",
+    price: 229.5,
+    title: "Floral Print High Heeled Shoes",
+    description:
+      "Elevate your shoe game with these stunning high-end high-heeled shoes, featuring a beautiful floral design set against a rich blue background. These shoes are the perfect combination of fashion-forward design and high-quality craftsmanship, making them a must-have addition to any shoe collection.",
+    },
+];
+
+// Populate product inventory with sample data
+const allProducts = [];
+
+for (let data of sampleProductData) {
+  const { id, price, title, description} = data;
+  // Generate random in-stock quantity
+  const units = Math.floor(Math.random() * 5);
+  allProducts.push(new Product(id, price, title, description, units));
+}
+
+// Choose and display random product
+const randomId = Math.floor(Math.random() * allProducts.length);
+productDetailsPlaceholder.innerHTML = allProducts[randomId].htmlContent;
--- modules/product.js
+++ modules/product.js
@@ -1,14 +1,26 @@
+// Product class
+// Represents one Product
 export class Product {
     constructor(id, price, title, description, units) {
         this.id = id;
         this.price = price;
         this.title = title;
         this.description = description;
+        this.units = units;
     }
 
-    static baseUrl = './resources/product-images/';
-
     get imageUrl() {
-        return `${this.baseUrl}${id}.png`;
+        const baseUrl = "./resources/product-images/";
+        return `${baseUrl}${this.id}.jpg`;
+    }
+
+    get htmlContent() {
+        return `
+            <img class='product__image--large' src='${this.imageUrl}'/>
+            <div class="product__details stack-medium">
+                <h2>${this.title}</h2>
+                <p>${this.description}</p>
+            </div>
+            `;
     }
 }
