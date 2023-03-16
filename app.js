97907b520ae0f0f0026c91b6bcc6226305a97c6b
Mark hanley <home@markhanley.co.uk>
2023-03-16 20:29:39 +0000

    Refactor html display methods

--- app.js
+++ app.js
@@ -5,15 +5,17 @@ import { Basket } from "./modules/basket.js";
 import { sampleProductData } from "./test-data.js";
 
 // Get page element references
-const productDetailsPlaceholder = document.querySelector(
-  "#product-details-placeholder"
-);
+const imageElement = document.querySelector("#product-image");
+const headingElement = document.querySelector("#product-heading");
+const descriptionElement = document.querySelector("#product-description");
+const priceElement = document.querySelector("#product-price");
+const stockElement = document.querySelector("#product-stock");
 
 // Populate product inventory with sample data
 const allProducts = [];
 
 for (let data of sampleProductData) {
-  const { id, price, title, description} = data;
+  const { id, price, title, description } = data;
   // Generate random in-stock quantity
   const units = Math.floor(Math.random() * 5);
   allProducts.push(new Product(id, price, title, description, units));
@@ -21,4 +23,11 @@ for (let data of sampleProductData) {
 
 // Choose and display random product
 const randomId = Math.floor(Math.random() * allProducts.length);
-productDetailsPlaceholder.innerHTML = allProducts[randomId].htmlContent;
+const product = allProducts[randomId];
+
+product.populateImage(imageElement);
+product.populateHeading(headingElement);
+product.populateDescription(descriptionElement);
+product.populatePrice(priceElement);
+product.populateStock(stockElement);
+
--- modules/product.js
+++ modules/product.js
@@ -1,33 +1,41 @@
 // Product class
 // Represents one Product
 export class Product {
-    constructor(id, price, title, description, units) {
-        this.id = id;
-        this.price = price;
-        this.title = title;
-        this.description = description;
-        this.units = units;
-    }
+  constructor(id, price, title, description, units) {
+    this.id = id;
+    this.price = price;
+    this.title = title;
+    this.description = description;
+    this.units = units;
+  }
 
-    get imageUrl() {
-        const baseUrl = "./resources/product-images/";
-        return `${baseUrl}${this.id}.jpg`;
-    }
+  get imageUrl() {
+    const baseUrl = "./resources/product-images/";
+    return `${baseUrl}${this.id}.jpg`;
+  }
 
-    get priceFormatted() {
-        return this.price.toFixed(2);
-    }
+  get priceFormatted() {
+    return this.price.toFixed(2);
+  }
 
-    get htmlContent() {
-        let html = `
-            <img class='product__image--large' src='${this.imageUrl}'/>
-            <div class="product__details stack-large">
-                <h2>${this.title}</h2>
-                <p>${this.description}</p>
-                <p class='product__price'>£${this.priceFormatted}</p>
-            </div>
-            `;
+  populateImage(imgElement) {
+    imgElement.src = this.imageUrl;
+    imgElement.alt = `Picture of ${this.title}`;
+  }
 
-            return html;
-    }
+  populateHeading(hElement) {
+    hElement.textContent = this.title;
+  }
+
+  populateDescription(pElement) {
+    pElement.textContent = this.description;
+  }
+
+  populateStock(pElement) {
+    pElement.textContent = `${this.units} IN STOCK`;
+  }
+
+  populatePrice(pElement) {
+    pElement.textContent = `£${this.priceFormatted}`;
+  }
 }
